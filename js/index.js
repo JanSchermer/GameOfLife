c6e5d08c26a8281a557825002befa38b545fefef
Jan Schermer <jan@jans-development.de>
2021-01-20 01:48:16 +0100

    Route Implemetation

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import Editor from './editor.js';
 import Brush from './tools/brush.js';
 import Line from './tools/line.js';
 import Rectangle from './tools/rectangle.js'
+import Route from './tools/route.js'
 
 function main(){
 
@@ -12,7 +13,8 @@ function main(){
 
   //new Brush({radius: 1, object: 1, round: true}).activate();
   //new Line({object: 1, density:100});
-  new Rectangle({object: 1, filled: true, density:20}).activate();
+  //new Rectangle({object: 1, filled: true, density:20}).activate();
+  new Route({object: 1, filled: false, density:100}).activate();
 
 }
 
---
+++ js/tools/route.js
@@ -0,0 +1,55 @@
+import Board from '../board.js';
+import Tool from './tool.js';
+
+export default class Route extends Tool {
+
+  // Options: object:num, filled:bool, density:num(0-100)
+  constructor(options){
+    super(options);
+  }
+
+  mouseMove(event) {
+    if(this.isMouseDown)
+      this.draw(this.drawBackgroud);
+  }
+
+  mouseRelease(event){
+    this.draw(this.drawObject)
+  }
+
+  draw(drawMethod){
+    const board = Board.current;
+
+    const height = this.absSelectEndY - this.absSelectStartY
+    const width = this.absSelectEndX - this.absSelectStartX
+
+    const centerX = this.absSelectStartX + width / 2
+    const centerY = this.absSelectStartY + height / 2
+
+    for(let y = this.absSelectStartY; y <= this.absSelectEndY; y++){
+      for(let x = this.absSelectStartX; x <= this.absSelectEndX; x++){
+
+        const xOffset = Math.abs(centerX - x);
+        const yOffset = Math.abs(centerY - y);
+        
+        const minX = width / 2 * (1 - yOffset / (height / 2))
+        const minY = height / 2 * (1 - xOffset / (width / 2))
+
+        if(xOffset < minX && yOffset < minY){
+          if(this.options.filled || Math.ceil(xOffset) == Math.floor(minX) || Math.ceil(yOffset) == Math.floor(minY))
+            drawMethod(board, this.options, x, y);
+        }
+      }   
+    }
+  }
+
+  drawObject(board, options, x, y) {
+    if(Math.random() * 100 < options.density)
+      board.items[y][x] = options.object;
+  }
+
+  drawBackgroud(board, options, x, y){
+    board.background[y][x] = "#5193fc";
+  }
+
+}
